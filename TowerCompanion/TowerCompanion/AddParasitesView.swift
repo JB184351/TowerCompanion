9df0a9de12400589f7fda316d08f28cb8de9e03f
JB184351 <justinbengtson@icloud.com>
2023-06-28 18:13:22 -0600

    Removed Weapon variables from AddRunView

---
+++ TowerCompanion/TowerCompanion/AddParasitesView.swift
@@ -0,0 +1,18 @@
+//
+//  AddParasitesView.swift
+//  TowerCompanion
+//
+//  Created by Justin on 6/28/23.
+//
+
+import SwiftUI
+
+struct AddParasitesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddParasitesView()
+}
--- TowerCompanion/TowerCompanion/AddRunView.swift
+++ TowerCompanion/TowerCompanion/AddRunView.swift
@@ -15,24 +15,6 @@ struct AddRunView: View {
     @State private var platform = ""
     private let platforms = ["PS5", "PC"]
     
-    // Weapon Related Variables
-    @State private var weaponName = ""
-    @State private var weaponLevel = 0
-    @State private var altFireName = ""
-    @State private var altFireLevel = 0
-    @State private var altFIreDescription = ""
-    @State private var weaponTraitLevel = 1
-    @State private var weaponTraitNamesUsedInRun = [String]()
-    @State private var weaponTraitName = ""
-    
-    private let weaponNames = ["Modified Sidearm SD-M8", "Hollowseeker", "Electropylon Driver", "Rotgland Lobber", "Pyroshell Caster", "Thermogenic Launcher", "Dreadbound", "Coilspine Shredder", "Tachyomatic Carbine", "Spitmaw Blaster"]
-    
-    @State private var weaponTraitNames = [String]()
-    
-    var altFires: [AltFire] {
-        return AddRunView.getAllAltFires()
-    }
-    
     // Parasites
     @State private var parasiteName = ""
     @State private var parasitePositiveEffectDescription = "Positive Effect description will go here"
