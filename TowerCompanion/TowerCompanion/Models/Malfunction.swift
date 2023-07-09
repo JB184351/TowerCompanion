25b5bc4c0228f979e312c46ad90fa616970fe102
JB184351 <justinbengtson@icloud.com>
2023-07-08 20:27:10 -0600

    Removed WeaponStat object, not using and don't see myself needing it anytime soon.

--- TowerCompanion/TowerCompanion/Models/Malfunction.swift
+++ TowerCompanion/TowerCompanion/Models/Malfunction.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // Adding Enum type on Object works with Xcode 15 beta 3
 // Wait until Beta 4 or whenever Live Previews are working
-// with SwiftUI before adding this property to the Malfunction Type
+// with SwiftData before adding this property to the Malfunction Type
 enum MalfunctionType: Codable, Hashable {
     case normal
     case permanent
