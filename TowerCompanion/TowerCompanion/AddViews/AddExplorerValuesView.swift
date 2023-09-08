8e0f4b83d3ed4363ba7561793f3130ccbf060cc4
JB184351 <justinbengtson@icloud.com>
2023-09-07 18:19:35 -0700

    Updated Explorer Values View to have a navigationView and Form

--- TowerCompanion/TowerCompanion/AddViews/AddExplorerValuesView.swift
+++ TowerCompanion/TowerCompanion/AddViews/AddExplorerValuesView.swift
@@ -26,40 +26,45 @@ struct AddExplorerValuesView: View {
     }()
     
     var body: some View {
-        Text("Enter Number of Floors cleared")
-        TextField("Enter the Number Here", value: $floorsCleared, formatter: numberFormatter)
-            .keyboardType(.numberPad)
-            .focused($textFieldFocus)
-            .onAppear {
-                explorerValues = Explorer(floorsCleared: 0, silphiumFound: 0, obolitesCollected: 0, calibratorsCollected: 0)
+        NavigationView {
+            Form {
+                Text("Enter Number of Floors cleared")
+                TextField("Enter the Number Here", value: $floorsCleared, formatter: numberFormatter)
+                    .keyboardType(.numberPad)
+                    .focused($textFieldFocus)
+                    .onAppear {
+                        explorerValues = Explorer(floorsCleared: 0, silphiumFound: 0, obolitesCollected: 0, calibratorsCollected: 0)
+                    }
+                    .onChange(of: floorsCleared) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                    }
+                
+                Text("Enter Number of Silphiums found")
+                TextField("Enter the Number Here", value: $silphiumFound, formatter: numberFormatter)
+                    .keyboardType(.numberPad)
+                    .focused($textFieldFocus)
+                    .onChange(of: silphiumFound) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                    }
+                
+                Text("Enter the number of Obolites collected")
+                TextField("Enter the Number Here", value: $obolitesCollected, formatter: numberFormatter)
+                    .keyboardType(.numberPad)
+                    .focused($textFieldFocus)
+                    .onChange(of: obolitesCollected) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                    }
+                
+                Text("Enter the Number of Calibrators collected")
+                TextField("Enter the Number Here", value: $calibratorsCollected, formatter: numberFormatter)
+                    .keyboardType(.numberPad)
+                    .focused($textFieldFocus)
+                    .onChange(of: calibratorsCollected) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                    }
             }
-            .onChange(of: floorsCleared) { oldValue, newValue in
-                explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
-            }
-        
-        Text("Enter Number of Silphiums found")
-        TextField("Enter the Number Here", value: $silphiumFound, formatter: numberFormatter)
-            .keyboardType(.numberPad)
-            .focused($textFieldFocus)
-            .onChange(of: silphiumFound) { oldValue, newValue in
-                explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
-            }
-        
-        Text("Enter the number of Obolites collected")
-        TextField("Enter the Number Here", value: $obolitesCollected, formatter: numberFormatter)
-            .keyboardType(.numberPad)
-            .focused($textFieldFocus)
-            .onChange(of: obolitesCollected) { oldValue, newValue in
-                explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
-            }
-        
-        Text("Enter the Number of Calibrators collected")
-        TextField("Enter the Number Here", value: $calibratorsCollected, formatter: numberFormatter)
-            .keyboardType(.numberPad)
-            .focused($textFieldFocus)
-            .onChange(of: calibratorsCollected) { oldValue, newValue in
-                explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
-            }
+            .navigationTitle("Explorer Values")
+        }
     }
 }
 
