0a1f406fd2e5c6c8abb3714a1f93574778dd7b4c
JB184351 <justinbengtson@icloud.com>
2023-09-21 17:54:16 -0700

    Changed how Add Parasites view worked to be more like EditParasitesView.

--- TowerCompanion/TowerCompanion/AddViews/AddParasitesView.swift
+++ TowerCompanion/TowerCompanion/AddViews/AddParasitesView.swift
@@ -8,15 +8,11 @@
 import SwiftUI
 
 struct AddParasitesView: View {
-    @State private var parasiteName = ""
     @State private var parasitePositiveEffectDescription = "Positive Effect description will go here"
     @State private var parasiteNegativeEffectDescription = "Negative Effect description will go here"
-    @State private var numberOfParasites = 1
-    @State private var isPlaceHolderEnabled = true
-    @State private var parasiteNamesUsedInRun = [String]()
-    @State private var pickerCount = 1
-    @State private var nonComputedParasiteNames = [String]()
-    @State private var isFirstView = false
+    @State private var currentlySelectedParasiteName = "Amenesic Firestinger"
+    @State private var allParasiteNames: [String] = []
+    @State private var isFirstView = true
     @Binding var parasites: [Parasite]
     
     private var parasiteNames: [String] {
@@ -24,88 +20,86 @@ struct AddParasitesView: View {
     }
     
     init(parasites: Binding<[Parasite]>) {
-        _parasiteNamesUsedInRun = State(initialValue: Array(repeating: "", count: 1))
-        _nonComputedParasiteNames = State(initialValue: Array(repeating: "", count: 1))
         self._parasites = parasites
     }
     
     var body: some View {
-        ForEach(0..<1, id: \.self) { index in
-            Picker("Parasites", selection: Binding(
-                get: { nonComputedParasiteNames[index] },
-                set: { nonComputedParasiteNames[index] = $0 }
-            )) {
-                ForEach(nonComputedParasiteNames, id:\.self) { parasiteName in
+        Section {
+            Picker("Parasites", selection: $currentlySelectedParasiteName) {
+                ForEach(allParasiteNames, id:\.self) { parasiteName in
                     Text(parasiteName).tag(parasiteName)
                 }
             }
-            // Some reason the parasites array gets 1 item
-            // inserted so I'm removing it forcibly until I
-            // can figure out why that is
-            .onAppear {
-                if !isFirstView {
-                    parasites.removeAll()
-                    nonComputedParasiteNames = parasiteNames
-                    isFirstView = true
-                }
+            .onChange(of: currentlySelectedParasiteName) {
+                parasitePositiveEffectDescription = getParasiteEffectDescriptions(parasiteName: currentlySelectedParasiteName).0
+                parasiteNegativeEffectDescription = getParasiteEffectDescriptions(parasiteName: currentlySelectedParasiteName).1
             }
-            .onChange(of: nonComputedParasiteNames[index]) {
-                isPlaceHolderEnabled = false
-                parasitePositiveEffectDescription = getParasiteEffectDescriptions(parasiteName: nonComputedParasiteNames[index]).0
-                parasiteNegativeEffectDescription = getParasiteEffectDescriptions(parasiteName: nonComputedParasiteNames[index]).1
-            }
-        }
-        
-        Text(parasitePositiveEffectDescription)
-            .foregroundStyle(isPlaceHolderEnabled ? .gray.opacity(0.5) : .green)
-        Text(parasiteNegativeEffectDescription)
-            .foregroundStyle(isPlaceHolderEnabled ? .gray.opacity(0.5) : .red)
-        
-        Section {
-            Button("Add Parasite") {
-                withAnimation {
-                    addParasite()
-                }
-            }
-            .disabled(parasites.count == 5)
             
-            Button("Remove Selected Parasites") {
-                withAnimation {
-                    clearParasites()
-                }
-            }
-            .disabled(parasites.count < 1)
-        }
-        
-        if parasites.count > 0 {
+            Text(parasitePositiveEffectDescription)
+                .foregroundStyle(.green)
+            Text(parasiteNegativeEffectDescription)
+                .foregroundStyle(.red)
+            
             Section {
-                ForEach(parasites, id: \.self) { parasite in
-                    Text(parasite.name)
-                    Text(parasite.positiveDescription)
-                        .foregroundStyle(.green)
-                    Text(parasite.negativeDescription)
-                        .foregroundStyle(.red)
+                Button("Add Parasite") {
+                    withAnimation {
+                        addParasite()
+                    }
                 }
-            } header: {
-                Text("Parasites")
+                .disabled(parasites.count == 5)
+                
+                Button("Remove Selected Parasites") {
+                    withAnimation {
+                        clearParasites()
+                    }
+                }
+                .disabled(parasites.count < 1)
             }
-        } else {
-            EmptyView()
+            
+            if parasites.count > 0 {
+                Section {
+                    ForEach(parasites, id: \.self) { parasite in
+                        Text(parasite.name)
+                        Text(parasite.positiveDescription)
+                            .foregroundStyle(.green)
+                        Text(parasite.negativeDescription)
+                            .foregroundStyle(.red)
+                    }
+                } header: {
+                    parasites.count > 0 ? Text("Parasites") : Text("")
+                }
+            } else {
+                EmptyView()
+            }
+        }
+        .onAppear {
+            
+            // Still getting the weird bug about a random parasite
+            // being inserted somehow.
+            if isFirstView {
+                parasites.removeAll()
+                isFirstView = false
+            }
+            
+            allParasiteNames = parasiteNames
+            
+            currentlySelectedParasiteName = allParasiteNames[0]
+            parasitePositiveEffectDescription = getParasiteEffectDescriptions(parasiteName: currentlySelectedParasiteName).0
+            parasiteNegativeEffectDescription = getParasiteEffectDescriptions(parasiteName: currentlySelectedParasiteName).1
         }
     }
     
     private func addParasite() {
-        for (index, _) in parasiteNamesUsedInRun.enumerated() {
-            let parasite = Parasite(name: nonComputedParasiteNames[index], positiveDescription: parasitePositiveEffectDescription, negativeDescription: parasiteNegativeEffectDescription)
-            parasites.append(parasite)
-            
-            nonComputedParasiteNames.remove(at: index)
-        }
+        let parasite = Parasite(name: currentlySelectedParasiteName, positiveDescription: parasitePositiveEffectDescription, negativeDescription: parasiteNegativeEffectDescription)
+        parasites.append(parasite)
+        
+        allParasiteNames.removeAll(where: { $0 == parasite.name })
     }
     
     private func clearParasites() {
         parasites.removeAll()
-        nonComputedParasiteNames = parasiteNames
+        allParasiteNames = parasiteNames
+        currentlySelectedParasiteName = allParasiteNames[0]
     }
     
     private func getParasiteEffectDescriptions(parasiteName: String) -> (String, String) {
