0eebee319dd5fca5e7b6923461e2730751a3641d
JB184351 <justinbengtson@icloud.com>
2023-09-21 17:53:28 -0700

    Applied the same changes made in Add Malfunction view to the Edit version.

--- TowerCompanion/TowerCompanion/EditViews/EditMalfunctionsView.swift
+++ TowerCompanion/TowerCompanion/EditViews/EditMalfunctionsView.swift
@@ -11,17 +11,36 @@ struct EditMalfunctionsView: View {
     @State private var isPermanent = false
     @State private var malfunctionDescription = ""
     @State private var malfunctionRemovalCondition = ""
-    @State private var isFirstView = false
+    @State private var listOfPermanentMalfunctions: [String] = []
+    @State private var shouldShowMaxNumberOfRegularMalfunctionsAlert = false
+    @State private var shouldShowMaxNumberOfPermanentMalfunctionsAlert = false
+    @State private var shouldShowMaxNumberOfMalfunctionsAlert = false
+    @State private var hasShownMaxPermanentAlert = false
+    @State private var hasShownMaxRegularAlert = false
     @Binding var malfunctions: [Malfunction]
+    
+    var permanentMalfunctions: [String] {
+        Malfunction.getAllPermanentMalfunctions()
+    }
     
     var body: some View {
         Section {
             Toggle("Is this a permanent Malfunction?", isOn: $isPermanent)
-            
-            TextField("Enter the Malfunction Description", text: $malfunctionDescription)
+                .disabled(isTooManyPermanentMalfunctions())
+            if isPermanent {
+                Picker("Malfunctions", selection: $malfunctionDescription) {
+                    ForEach(listOfPermanentMalfunctions, id:\.self) { malfunction in
+                        Text(malfunction).tag(malfunction)
+                    }
+                }
+                
+            }
             
             if !isPermanent {
+                TextField("Enter the Malfunction Description", text: $malfunctionDescription)
+                    .disabled(isTooManyRegularMalfunctions())
                 TextField("Enter Malfunction Removal Condition", text: $malfunctionRemovalCondition)
+                    .disabled(isTooManyRegularMalfunctions())
             }
             
             Button("Add Malfunction") {
@@ -29,7 +48,10 @@ struct EditMalfunctionsView: View {
                     addMalfunction()
                 }
             }
-            .disabled(malfunctions.count == 7 || malfunctionDescription.isEmpty)
+            .disabled(disableAddMalfunctionButton())
+            .alert("Max number of (2) regular malfunctions have been entered and TextFields will now be disabled", isPresented: $shouldShowMaxNumberOfRegularMalfunctionsAlert) { Button("OK", role: .cancel) { hasShownMaxRegularAlert = true } }
+            .alert("Max number of (5) permanent malfunctions have been entered", isPresented: $shouldShowMaxNumberOfPermanentMalfunctionsAlert) { Button("OK", role: .cancel) { hasShownMaxPermanentAlert = true } }
+            .alert("Max number of malfunctions (7) have been entered", isPresented: $shouldShowMaxNumberOfMalfunctionsAlert) { }
             
             Button("Clear All Malfunctions") {
                 withAnimation {
@@ -37,19 +59,38 @@ struct EditMalfunctionsView: View {
                 }
             }
             .disabled(malfunctions.count < 1)
-            
-            Section {
-                ForEach(malfunctions, id: \.self) { malfunction in
-                    Text(malfunction.malfunctionType == .normal ? "Malfucntion: \(malfunction.malfunctionDescription) \nCondition To Remove: \(malfunction.conditionToRemove)" : malfunction.malfunctionDescription)
-                        .multilineTextAlignment(.leading)
+        }
+        // Some reason the malfunctions array gets 1 item
+        // inserted so I'm removing it forcibly until I
+        // can figure out why that is
+        .onAppear {
+            if malfunctions.count > 0 {
+                let permanentMalfunctions = malfunctions.filter({ $0.malfunctionType == .permanent })
+                
+                for malfunction in permanentMalfunctions {
+                    if listOfPermanentMalfunctions.contains(malfunction.malfunctionDescription) {
+                        listOfPermanentMalfunctions.removeAll(where: { $0 == malfunction.malfunctionDescription })
+                    }
                 }
-                .onDelete(perform: { indexSet in
-                    malfunctions.remove(atOffsets: indexSet)
-                })
+            } else {
+                listOfPermanentMalfunctions = permanentMalfunctions
             }
-            .onChange(of: isPermanent) { oldValue, newValue in
+        }
+        .onChange(of: isPermanent) { oldValue, newValue in
+            malfunctionDescription = isPermanent ? listOfPermanentMalfunctions[0] : ""
+            
+            if isPermanent {
                 malfunctionRemovalCondition = ""
             }
+        }
+        
+        Section {
+            ForEach(malfunctions, id: \.self) { malfunction in
+                Text(malfunction.malfunctionType == .normal ? "Malfunction: \(malfunction.malfunctionDescription) \nCondition To Remove: \(malfunction.conditionToRemove)" : malfunction.malfunctionDescription)
+                    .multilineTextAlignment(.leading)
+            }
+        } header: {
+            malfunctions.count > 0 ? Text("Malfunctions") : Text("")
         }
     }
     
@@ -59,12 +100,47 @@ struct EditMalfunctionsView: View {
         let malfunction = Malfunction(malfunctionDescription: malfunctionDescription, conditionToRemove: malfunctionRemovalCondition, malfunctionType: malfunctionType)
         malfunctions.append(malfunction)
         
-        malfunctionDescription = ""
+        if isTooManyPermanentMalfunctions() && !hasShownMaxPermanentAlert {
+            isPermanent = false
+            shouldShowMaxNumberOfPermanentMalfunctionsAlert = true
+        }
+        
+        if isTooManyRegularMalfunctions() && !hasShownMaxRegularAlert {
+            shouldShowMaxNumberOfRegularMalfunctionsAlert = true
+        }
+        
+        if malfunctions.count == 7 {
+            shouldShowMaxNumberOfMalfunctionsAlert = true
+        }
+        
+        listOfPermanentMalfunctions.removeAll(where: { $0 == malfunctionDescription })
+        
+        malfunctionDescription = isPermanent ? listOfPermanentMalfunctions[0] : ""
         malfunctionRemovalCondition = ""
     }
     
     private func reset() {
         malfunctions.removeAll()
+        listOfPermanentMalfunctions = permanentMalfunctions
+        malfunctionDescription = isPermanent ? listOfPermanentMalfunctions[0] : ""
+        hasShownMaxRegularAlert = false
+        hasShownMaxPermanentAlert = false
+    }
+    
+    private func disableAddMalfunctionButton() -> Bool {
+        if isPermanent {
+            return malfunctionDescription.isEmpty
+        } else {
+            return malfunctionDescription.isEmpty || malfunctionRemovalCondition.isEmpty
+        }
+    }
+    
+    private func isTooManyPermanentMalfunctions() -> Bool {
+        malfunctions.filter( { $0.malfunctionType == .permanent }).count == 5
+    }
+    
+    private func isTooManyRegularMalfunctions() -> Bool {
+        malfunctions.filter( { $0.malfunctionType == .normal }).count == 2
     }
 }
 
