17ccf67cb4864bbbf57f3352ce18d970b23a6bdf
JB184351 <justinbengtson@icloud.com>
2023-09-22 16:35:28 -0700

    Added a permanent malfunction and made correction to the other descriptions.

--- TowerCompanion/TowerCompanion/Models/Malfunction.swift
+++ TowerCompanion/TowerCompanion/Models/Malfunction.swift
@@ -18,15 +18,16 @@ struct Malfunction: Codable, Hashable {
     var malfunctionType: MalfunctionType
     
     static func getAllPermanentMalfunctions() -> [String] {
-        let obolitesDisappear = "Obolites disappear 1.5s faster"
+        let obolitesDisappear = "Obolites disappear 1.5s faster."
         let loseObolites = "Lose Oboloites when suffering damage."
-        let midAir = "While in midair, -50% Weapon Damage"
+        let midAir = "While in midair, Weapon Damage reduced by 50%."
         let counterOnHit = "Hostiles counter on hit."
         let meleeCooldown = "Melee cooldown increased by 2s."
-        let stationary = "While stationary, Weapon Damage reduced by 75%"
-        let reduceIntegrity = "Max Integrity reduced."
+        let stationary = "While stationary, Weapon Damage reduced by 75%."
+        let reduceIntegrity = "Reduced Max Integrity."
+        let acid = "Hostiles leave pools of acid upon death."
         
-        let permanentMalfunctions = [obolitesDisappear, loseObolites, midAir, counterOnHit, meleeCooldown, stationary, reduceIntegrity]
+        let permanentMalfunctions = [obolitesDisappear, loseObolites, midAir, counterOnHit, meleeCooldown, stationary, reduceIntegrity, acid]
         
         return permanentMalfunctions
     }
