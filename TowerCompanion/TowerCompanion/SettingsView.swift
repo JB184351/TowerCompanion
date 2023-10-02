e09a6d735744d954e36e8e007f5bf88fb69e6aa0
JB184351 <justinbengtson@icloud.com>
2023-10-02 16:55:55 -0700

    Changing how we get to the TipView

--- TowerCompanion/TowerCompanion/SettingsView.swift
+++ TowerCompanion/TowerCompanion/SettingsView.swift
@@ -9,57 +9,15 @@ import SwiftUI
 import StoreKit
 
 struct SettingsView: View {
-    @State private var showTips = false
-    @State private var showThanks = false
-    @StateObject private var store = TipStore()
-    @Environment(\.dismiss) private var dismiss
-    
+
     var body: some View {
-        VStack {
-            Button("Tip Me") {
-                showTips.toggle()
+        NavigationView {
+            NavigationLink("Support the app") {
+                TipView()
             }
-            .tint(.blue)
-            .buttonStyle(.bordered)
+            .navigationTitle("Settings")
+            .frame(alignment: .leading)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .overlay {
-            if showTips {
-                Color.black.opacity(0.8)
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-                    .onTapGesture {
-                        showTips.toggle()
-                    }
-                
-                TipsView {
-                    showTips.toggle()
-                }
-                .environmentObject(store)
-                .transition(.move(edge: .bottom).combined(with: .opacity))
-            }
-        }
-        .overlay(alignment: .bottom) {
-            if showThanks {
-                ThanksView {
-                    showThanks = false
-                }
-                .transition(.move(edge: .bottom).combined(with: .opacity))
-            }
-        }
-        .animation(.spring(), value: showTips)
-        .animation(.spring(), value: showThanks)
-        .onChange(of: store.action, { oldValue, newValue in
-            if newValue == .successful {
-                showTips = false
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    showThanks = true
-                    store.reset()
-                }
-            }
-        })
-        .alert(isPresented: $store.hasError, error: store.error) {}
     }
 }
 
---
+++ TowerCompanion/TowerCompanion/TipView.swift
@@ -0,0 +1,69 @@
+//
+//  TipView.swift
+//  TowerCompanion
+//
+//  Created by Justin on 9/30/23.
+//
+
+import SwiftUI
+
+struct TipView: View {
+    @State private var showTips = false
+    @State private var showThanks = false
+    @StateObject private var store = TipStore()
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Tip Me") {
+                    showTips.toggle()
+                }
+                .tint(.blue)
+                .buttonStyle(.bordered)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay {
+                if showTips {
+                    Color.black.opacity(0.8)
+                        .ignoresSafeArea()
+                        .transition(.opacity)
+                        .onTapGesture {
+                            showTips.toggle()
+                        }
+                    
+                    TipsView {
+                        showTips.toggle()
+                    }
+                    .environmentObject(store)
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                }
+            }
+            .overlay(alignment: .bottom) {
+                if showThanks {
+                    ThanksView {
+                        showThanks = false
+                    }
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                }
+            }
+            .animation(.spring(), value: showTips)
+            .animation(.spring(), value: showThanks)
+            .onChange(of: store.action, { oldValue, newValue in
+                if newValue == .successful {
+                    showTips = false
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        showThanks = true
+                        store.reset()
+                    }
+                }
+            })
+        .alert(isPresented: $store.hasError, error: store.error) {}
+        }
+    }
+}
+
+#Preview {
+    TipView()
+}
