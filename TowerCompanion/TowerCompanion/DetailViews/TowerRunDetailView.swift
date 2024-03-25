f225bb70ce94c8de95cf52e54d0c2082234b586b
JB184351 <justinbengtson@icloud.com>
2024-03-25 16:22:03 -0700

    Grouped items in TowerRunDetailView to get rid of the Spacers that were being used, I think this is cleaner.

--- TowerCompanion/TowerCompanion/DetailViews/TowerRunDetailView.swift
+++ TowerCompanion/TowerCompanion/DetailViews/TowerRunDetailView.swift
@@ -15,70 +15,68 @@ struct TowerRunDetailView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(alignment: .leading) {
-                    Spacer()
+                VStack(alignment: .leading, spacing: 20) {
+                    VStack(alignment: .leading) {
+                        HeavyTextHeadline(text: "Scout Name: \(towerRun.scoutName)", color: .returnalYellow)
+                        HeavyTextHeadline(text: "Platform: \(towerRun.platform)", color: .returnalYellow)
+                    }
+
+                    VStack(alignment: .leading) {
+                        HeavyTextHeadline(text: "Score: \(towerRun.score.formatted())", color: .returnalLightBlue)
+                        HeavyTextHeadline(text: "Phase: \(towerRun.phase)", color: .returnalLightBlue)
+                        HeavyTextHeadline(text: "Room: \(towerRun.room)", color: .returnalLightBlue)
+                    }
                     
-                    HeavyTextHeadline(text: "Scout Name: \(towerRun.scoutName)", color: .returnalYellow)
-                    HeavyTextHeadline(text: "Platform: \(towerRun.platform)", color: .returnalYellow)
+                    VStack(alignment: .leading) {
+                        HeavyTextHeadline(text: "Final Multiplier: \(towerRun.finalMultiplier)%", color: .returnalLightBlue)
+                        HeavyTextHeadline(text: "Average Multiplier: \(towerRun.averageMultiplier)%", color: .returnalLightBlue)
+                        HeavyTextHeadline(text: "Highest Multiplier: \(towerRun.highestMultiplier)%", color: .returnalLightBlue)
+                    }
+                 
+                    VStack(alignment: .leading) {
+                        HeavyTextHeadline(text: "Date Started: \(towerRun.dateStarted.formatted())", color: .returnalLightBlue)
+                        HeavyTextHeadline(text: "Date Completed: \(towerRun.dateCompleted.formatted())", color: .returnalLightBlue)
+                    }
                     
-                    Spacer()
-                    
-                    HeavyTextHeadline(text: "Score: \(towerRun.score.formatted())", color: .returnalLightBlue)
-                    HeavyTextHeadline(text: "Phase: \(towerRun.phase)", color: .returnalLightBlue)
-                    HeavyTextHeadline(text: "Room: \(towerRun.room)", color: .returnalLightBlue)
-                    
-                    Spacer()
-                    
-                    HeavyTextHeadline(text: "Final Multiplier: \(towerRun.finalMultiplier)%", color: .returnalLightBlue)
-                    HeavyTextHeadline(text: "Average Multiplier: \(towerRun.averageMultiplier)%", color: .returnalLightBlue)
-                    HeavyTextHeadline(text: "Highest Multiplier: \(towerRun.highestMultiplier)%", color: .returnalLightBlue)
-                    
-                    Spacer()
-                    
-                    HeavyTextHeadline(text: "Date Started: \(towerRun.dateStarted.formatted())", color: .returnalLightBlue)
-                    HeavyTextHeadline(text: "Date Completed: \(towerRun.dateCompleted.formatted())", color: .returnalLightBlue)
-                    
-                    Spacer()
-                    WeaponDetailView(weapon: towerRun.weapon)
-                    
-                    Spacer()
-                    if let artifacts = towerRun.artifacts {
-                        if artifacts.count > 0 {
-                            ArtifactsDetailView(artifacts: artifacts)
+                    VStack(alignment: .leading)  {
+                        WeaponDetailView(weapon: towerRun.weapon)
+                        
+                        if let artifacts = towerRun.artifacts {
+                            if artifacts.count > 0 {
+                                ArtifactsDetailView(artifacts: artifacts)
+                            }
                         }
-                    }
-                    
-                    Spacer()
-                    if let parasites = towerRun.parasites {
-                        if parasites.count > 0 {
-                            ParasitesDetailView(parasites: parasites)
+                        
+                        
+                        if let parasites = towerRun.parasites {
+                            if parasites.count > 0 {
+                                ParasitesDetailView(parasites: parasites)
+                            }
                         }
-                    }
-                    
-                    Spacer()
-                    if let malfunctions = towerRun.malfunctions {
-                        if malfunctions.count > 0 {
-                            MalfunctionsDetailView(malfunctions: malfunctions)
+                        
+                        if let malfunctions = towerRun.malfunctions {
+                            if malfunctions.count > 0 {
+                                MalfunctionsDetailView(malfunctions: malfunctions)
+                            }
                         }
-                    }
-                    
-                    Spacer()
-                    StatsDetailView(stats: towerRun.stats)
-                    
-                    if let combatValues = towerRun.combat {
-                        CombatDetailView(combat: combatValues)
-                    }
-                    
-                    if let explorer = towerRun.explorer {
-                        ExplorerDetailsView(explorer: explorer)
-                    }
-                    
-                    if let skill = towerRun.skill {
-                        SkillDetailView(skill: skill)
-                    }
-                    
-                    if let objectives = towerRun.objectives {
-                        ObjectivesDetailView(objectives: objectives)
+                        
+                        StatsDetailView(stats: towerRun.stats)
+                        
+                        if let combatValues = towerRun.combat {
+                            CombatDetailView(combat: combatValues)
+                        }
+                        
+                        if let explorer = towerRun.explorer {
+                            ExplorerDetailsView(explorer: explorer)
+                        }
+                        
+                        if let skill = towerRun.skill {
+                            SkillDetailView(skill: skill)
+                        }
+                        
+                        if let objectives = towerRun.objectives {
+                            ObjectivesDetailView(objectives: objectives)
+                        }
                     }
                     
                 }
