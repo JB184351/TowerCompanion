79399aa6a29b2691022ae6643ad50fde685dd80e
JB184351 <justinbengtson@icloud.com>
2024-03-27 20:41:12 -0700

    Updated Explore Add/Edit views to

--- TowerCompanion/TowerCompanion/AddViews/AddExplorerValuesView.swift
+++ TowerCompanion/TowerCompanion/AddViews/AddExplorerValuesView.swift
@@ -8,66 +8,73 @@
 import SwiftUI
 
 struct AddExplorerValuesView: View {
-    @State private var floorsCleared = 0
-    @State private var silphiumFound = 0
-    @State private var obolitesCollected = 0
-    @State private var calibratorsCollected = 0
+    @State private var floorsCleared = "0"
+    @State private var silphiumFound = "0"
+    @State private var obolitesCollected = "0"
+    @State private var calibratorsCollected = "0"
     @Binding var explorerValues: Explorer?
     
-    @FocusState private var textFieldFocus: Bool
-    
-    let numberFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.zeroSymbol = ""
-        formatter.formatterBehavior = .default
-        
-        return formatter
-    }()
+    @FocusState private var textFieldFocus: FocusField?
     
     var body: some View {
         NavigationStack {
             Form {
                 Text("Enter the number of Floors cleared")
-                TextField("Enter value here", value: $floorsCleared, formatter: numberFormatter)
-                    .keyboardType(.numberPad)
-                    .focused($textFieldFocus)
+                TextField("Enter value here", text: $floorsCleared)
+                    .focused($textFieldFocus, equals: .int)
+                    .numbersOnly($floorsCleared)
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
                     .onAppear {
                         explorerValues = Explorer(floorsCleared: 0, silphiumFound: 0, obolitesCollected: 0, calibratorsCollected: 0)
                     }
                     .onChange(of: floorsCleared) { oldValue, newValue in
-                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                        explorerValues = Explorer(floorsCleared: Int(floorsCleared) ?? 0, silphiumFound: Int(silphiumFound) ?? 0, obolitesCollected: Int(obolitesCollected) ?? 0, calibratorsCollected: Int(calibratorsCollected) ?? 0)
                     }
                 
                 Text("Enter the number of Silphiums found")
-                TextField("Enter value here", value: $silphiumFound, formatter: numberFormatter)
-                    .keyboardType(.numberPad)
-                    .focused($textFieldFocus)
-                    .onChange(of: silphiumFound) { oldValue, newValue in
-                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                TextField("Enter value here", text: $silphiumFound)
+                    .focused($textFieldFocus, equals: .int)
+                    .numbersOnly($silphiumFound)
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
+                    .onChange(of: floorsCleared) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: Int(floorsCleared) ?? 0, silphiumFound: Int(silphiumFound) ?? 0, obolitesCollected: Int(obolitesCollected) ?? 0, calibratorsCollected: Int(calibratorsCollected) ?? 0)
                     }
                 
                 Text("Enter the number of Obolites collected")
-                TextField("Enter value here", value: $obolitesCollected, formatter: numberFormatter)
-                    .keyboardType(.numberPad)
-                    .focused($textFieldFocus)
-                    .onChange(of: obolitesCollected) { oldValue, newValue in
-                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                TextField("Enter value here", text: $obolitesCollected)
+                    .focused($textFieldFocus, equals: .int)
+                    .numbersOnly($obolitesCollected)
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
+                    .onChange(of: floorsCleared) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: Int(floorsCleared) ?? 0, silphiumFound: Int(silphiumFound) ?? 0, obolitesCollected: Int(obolitesCollected) ?? 0, calibratorsCollected: Int(calibratorsCollected) ?? 0)
                     }
                 
                 Text("Enter the number of Calibrators collected")
-                TextField("Enter value here", value: $calibratorsCollected, formatter: numberFormatter)
-                    .keyboardType(.numberPad)
-                    .focused($textFieldFocus)
-                    .onChange(of: calibratorsCollected) { oldValue, newValue in
-                        explorerValues = Explorer(floorsCleared: floorsCleared, silphiumFound: silphiumFound, obolitesCollected: obolitesCollected, calibratorsCollected: calibratorsCollected)
+                TextField("Enter value here", text: $calibratorsCollected)
+                    .focused($textFieldFocus, equals: .int)
+                    .numbersOnly($calibratorsCollected)
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
+                    .onChange(of: floorsCleared) { oldValue, newValue in
+                        explorerValues = Explorer(floorsCleared: Int(floorsCleared) ?? 0, silphiumFound: Int(silphiumFound) ?? 0, obolitesCollected: Int(obolitesCollected) ?? 0, calibratorsCollected: Int(calibratorsCollected) ?? 0)
                     }
             }
             .navigationTitle("Explorer Values")
             .toolbar {
                 ToolbarItem(placement: .keyboard) {
+                    Spacer()
+                }
+                
+                ToolbarItem(placement: .keyboard) {
                     Button("Done") {
-                        textFieldFocus = false
+                        textFieldFocus = nil
                     }
                 }
             }
